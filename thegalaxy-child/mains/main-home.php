d21fa6dfdec123532734dd9edb92ea31d7bb7c2c
Corey Sizemore <coreysizemore@gmail.com>
2018-10-15 09:15:05 -0700

    Child Theme Updates

--- thegalaxy-child/mains/main-home.php
+++ thegalaxy-child/mains/main-home.php
@@ -4,204 +4,146 @@
 		@package WordPress
 		@subpackage thegalaxy
 	*/
+	
+	if( get_field('secondary_navigation') ):
+	
+		if ( has_nav_menu( 'secondary_nav' ) ):
+		
+	    	echo get_template_part( 'misc/secondarynav' );
+	    	    	
+	    endif;
+    
+    endif;
+    
+    if ( is_active_sidebar( 'attention-sidebar-1' ) ):
 	 
-?>
+		get_template_part( 'sidebars/sidebar' , 'attentiontop' );
 	
-<?php 
+	endif;
 
 	if( get_field('display_call_out_boxes') ):
-
+	
 		get_template_part( 'misc/calloutboxes' );
-		
+	
 	endif;
-	
-?>
 
-<?php 
+	echo '<div id="main" class="';
 	
-	if( get_field('display_call_out_boxes') ):
+	echo basename(get_permalink());
+	
+	echo '">';
+	
+	if( get_field('default_editor')):
+	
+		echo '<div class="default_editor"><div class="container"><div class="row gutters">';
+	
+		if( get_field('sidebar_selection') == 'right' ):
+	
+			echo '<div class="col_8"><div class="content">';
+	
+			get_template_part( 'loops/loop', 'page' );
 
-		get_template_part( 'misc/calloutboxes' );
-		
-	endif;
-		
-?>
-
-<div id="main" class="<?php echo basename(get_permalink()); ?>">
-
-	<?php if( get_field('default_editor')): ?>
-	
-		<div class="default_editor">
-		
-			<div class="container">
-					
-				<div class="row gutters">
-						
-					<?php if( get_field('sidebar_selection') == 'right' ): ?>
-						
-						<div class="col_9">
-								
-							<div class="content">
-					
-								<?php get_template_part( 'loops/loop', 'page' ); ?>
-								
-								<?php
-	
-									if( get_field('gallery') ):
-								
-										get_template_part( 'misc/gallery' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('accordion') ):
-								
-										get_template_part( 'misc/accordion' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('tabs') ):
-								
-										get_template_part( 'misc/tabs' );
-										
-									endif;
-										
-								?>
-									
-							</div>
-								
-						</div>
-							
-						<div class="col_3">
-								
-							<?php get_template_part( 'sidebars/sidebar' , 'primary' ); ?>
-								
-						</div>
-					
-					<?php endif; ?>
-					
-					<?php if( get_field('sidebar_selection') == 'none' ): ?>
-					
-						<div class="col_12">
-							
-							<div class="content">
-				
-								<?php get_template_part( 'loops/loop', 'page' ); ?>
-								
-								<?php
-	
-									if( get_field('gallery') ):
-								
-										get_template_part( 'misc/gallery' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('accordion') ):
-								
-										get_template_part( 'misc/accordion' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('tabs') ):
-								
-										get_template_part( 'misc/tabs' );
-										
-									endif;
-										
-								?>
-								
-							</div>
-							
-						</div>
-					
-					<?php endif; ?>
-					
-					<?php if( get_field('sidebar_selection') == 'left' ): ?>
-					
-						<div class="col_3">
-								
-							<?php get_template_part( 'sidebars/sidebar' , 'primary' ); ?>
-							
-						</div>
-						
-						<div class="col_9">
-							
-							<div class="content">
-				
-								<?php get_template_part( 'loops/loop', 'page' ); ?>
-								
-								<?php
-	
-									if( get_field('gallery') ):
-								
-										get_template_part( 'misc/gallery' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('accordion') ):
-								
-										get_template_part( 'misc/accordion' );
-										
-									endif;
-										
-								?>
-								
-								<?php 
-									
-									if( get_field('tabs') ):
-								
-										get_template_part( 'misc/tabs' );
-										
-									endif;
-										
-								?>
-								
-							</div>
-							
-						</div>
-					
-					<?php endif; ?>
-						
-				</div>
-				
-			</div>
+			if( get_field('gallery') ):
 			
-		</div>
-	
-	<?php endif; ?>
-	
-	<?php if(is_user_logged_in()):?>
-	
-		<div class="edit_button">
-		
-			<span class="edit"><?php edit_post_link(); ?></span>
-		
-		</div>
-	
-	<?php endif; ?>
+				get_template_part( 'misc/gallery' );
+				
+			endif;
 
-</div>
+			if( get_field('accordion') ):
+			
+				get_template_part( 'misc/accordion' );
+				
+			endif;
 
-<?php
+			if( get_field('tabs') ):
+			
+				get_template_part( 'misc/tabs' );
+				
+			endif;
+
+			echo '</div></div><div class="col_4">';
+			
+			get_template_part( 'sidebars/sidebar' , 'primary' );
+			
+			echo '</div>';
+		
+		endif;
+	
+		if( get_field('sidebar_selection') == 'none' ):
+		
+			echo '<div class="col_12"><div class="content">';
+			
+			get_template_part( 'loops/loop', 'page' );
+			
+			if( get_field('gallery') ):
+			
+				get_template_part( 'misc/gallery' );
+				
+			endif;
+			
+			if( get_field('accordion') ):
+			
+				get_template_part( 'misc/accordion' );
+				
+			endif;
+		
+			if( get_field('tabs') ):
+			
+				get_template_part( 'misc/tabs' );
+				
+			endif;
+		
+			echo '</div></div>';
+		
+		endif;
+		
+		if( get_field('sidebar_selection') == 'left' ):
+		
+			echo '<div class="col_4">';
+			
+			get_template_part( 'sidebars/sidebar' , 'primary' );
+			
+			echo '</div><div class="col_8"><div class="content">';
+			
+			get_template_part( 'loops/loop', 'page' );
+		
+			if( get_field('gallery') ):
+			
+				get_template_part( 'misc/gallery' );
+				
+			endif;
+			
+			if( get_field('accordion') ):
+			
+				get_template_part( 'misc/accordion' );
+				
+			endif;
+		
+			if( get_field('tabs') ):
+			
+				get_template_part( 'misc/tabs' );
+				
+			endif;
+			
+			echo '</div></div>';
+		
+		endif;
+		
+		echo '</div></div></div>';
+	
+	endif;
+	
+	if(is_user_logged_in()):
+	
+		echo '<div class="edit_button"><span class="edit">';
+		
+		edit_post_link();
+		
+		echo '</span></div>';
+	
+	endif;
+	
+	echo '</div>';
 
 	if( have_rows('services_home') ):
 	
@@ -234,12 +176,8 @@
 		echo '</div></div></div>';
 		
 	endif;
-
-?>
-
-<?php get_template_part( 'misc/parallax' ); ?>
-
-<?php
+	
+	get_template_part( 'misc/parallax' );
 
 	if( get_field('attention_bottom' )):
 	
@@ -250,8 +188,4 @@
 		endif;
 		
 	endif;
-						
 ?>
-	
-	
-	
